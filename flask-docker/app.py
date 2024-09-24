30d781609968213f4b9704f21559b9c6d88c43d7
Seblb44 <sebastien.lebris@gmail.com>
2024-09-24 11:03:23 +0700

    Update app.py

--- flask-docker/app.py
+++ flask-docker/app.py
@@ -1,22 +1,14 @@
-from flask import Flask, render_template, request
-import random
-from redis import Redis
+from flask import Flask, jsonify
+import paho.mqtt.client as mqtt
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    color = "#"+''.join([random.choice('0123456789ABCDEF') for _ in range(6)])
-    if request.method == 'POST':
-        redis.incr('clicks')
-    clicks = redis.get('clicks')
-    if clicks is None:
-        clicks = 0
-    else:
-        clicks = clicks.decode()
-       
-    return render_template('index.html', color=color, clicks=clicks)
-        
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True) 
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to the Leafynovation control panel!"})
+
+# You can add more routes and functions here for controlling actuators, fetching data, etc.
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
